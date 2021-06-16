71df7c71373815203746a0bd0018eb39c784649e
lucianogiusti <lucianogiusti@outlook.com.ar>
2021-06-16 11:33:59 -0300

    active player

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 "use strict";
 
 // Selecting elements
-
+const player0El = document.querySelector(".player--0");
+const player1El = document.querySelector(".player--1");
 const score0El = document.getElementById("score--0");
 const score1El = document.getElementById("score--1");
 const current0El = document.getElementById("current--0");
-const current1El = document.getElementById("current-1");
+const current1El = document.getElementById("current--1");
 
 const diceEl = document.querySelector(".dice");
 const btnNewEl = document.querySelector(".btn--new");
@@ -18,7 +19,9 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add("hidden");
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 // Rolling dice functionality
 
@@ -34,8 +37,15 @@ btnRollEl.addEventListener("click", function () {
   // Check for rolled 1
   if (dice !== 1) {
     currentScore += dice;
-    current0El.textContent = currentScore; // CHANGE LATER
+    document.getElementById(
+      `current--${activePlayer}`
+    ).textContent = currentScore;
   } else {
     // Switch player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle("player--active");
+    player1El.classList.toggle("player--active");
   }
 });
