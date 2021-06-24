a3633e2b2b856fb523594fa2a420518d6f7e967d
lucianogiusti <lucianogiusti@outlook.com.ar>
2021-06-23 23:16:25 -0300

    final changes

--- script.js
+++ script.js
@@ -19,10 +19,23 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add("hidden");
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+let scores, currentScore, activePlayer, playing;
+
+const newGame = function () {
+scores = [0, 0];
+currentScore = 0;
+activePlayer = 0;
+playing = true;
+score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+};
+newGame();
 
 // Switch Player function
 
@@ -33,6 +46,15 @@ const switchPlayer = function () {
   player0El.classList.toggle("player--active");
   player1El.classList.toggle("player--active");
 };
+
+// Reseting Scores and Hiding Elements when clicking New game
+
+const resetScores = function() {
+  diceEl.classList.add("hidden");
+  btnRollEl.classList.remove("hidden");
+  btnHoldEl.classList.remove("hidden");
+}
+
 
 // Rolling dice functionality
 
@@ -75,9 +97,17 @@ btnHoldEl.addEventListener("click", function () {
       document
         .querySelector(`.player--${activePlayer}`)
         .classList.remove("player--active");
+      btnRollEl.classList.add("hidden");
+      btnHoldEl.classList.add("hidden");
     } else {
       //Switch player
       switchPlayer();
     }
   }
 });
+
+btnNewEl.addEventListener("click", function() {
+  newGame();
+  resetScores();
+  
+})
